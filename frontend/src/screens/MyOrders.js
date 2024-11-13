179d20b85160b4eda1f7f1070a5c39f4038612d6
SaiNaik033 <162146239+SaiNaik033@users.noreply.github.com>
2024-11-14 00:01:47 +0530

    Update MyOrders.js

--- frontend/src/screens/MyOrders.js
+++ frontend/src/screens/MyOrders.js
@@ -11,7 +11,7 @@ export default function MyOrder() {
         
         
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myOrderData", {
+        await fetch("https://munchmate-deploy-backend.onrender.com/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
